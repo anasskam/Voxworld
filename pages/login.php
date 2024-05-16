5c37b86f14d3d06dafebac2c37f7a3a718eb5f18
anasskam <anasskam361@gmail.com>
2024-05-16 05:04:27 +0100

    added the login and sign up pages

---
+++ pages/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Log in</title>
+</head>
+<body>
+  <div class="two-cols">
+    <div class="left">
+      <img src="#" alt="illustration">
+    </div>
+    
+    <div class="right">
+      <header>
+       <img src="../assets/images/logo.svg" alt="logo">
+      </header>
+
+      <form action="">
+        <header>
+          <h1>LOGIN</h1>
+          <p>The best place to discover news for free</p>
+        </header>
+        <div class="inputs">
+          <div class="email-input">
+            <img src="../assets/icons/email.svg" alt="email icon">
+            <input type="email" placeholder="Enter your email" required>
+          </div>
+
+          <div class="password-input">
+            <img src="../assets/icons/password.svg" alt="password icon">
+            <input type="password" placeholder="Enter your password" required>
+            <img src="../assets/icons/eye.svg" alt="show toggle icon">
+          </div>
+
+        </div>
+        <div class="cta">
+          <input type="submit" value="Login to my account">
+          <p>Don’t have an account? <a href="./register.php">Sign up</a></p>
+        </div>
+      </form>
+      <footer>
+        <div class="two-cols">
+          <p>© Copyright, 2024 All rights reserved to Voxworld </p>
+          <div class="right">
+            <ul>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Help</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+    
+
+  </div>
+</body>
+</html>
---
+++ pages/register.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Log in</title>
+</head>
+<body>
+  <div class="two-cols">
+    <div class="left">
+      <img src="#" alt="illustration">
+    </div>
+    
+    <div class="right">
+      <header>
+       <img src="../assets/images/logo.svg" alt="logo">
+      </header>
+
+      <form action="">
+        <header>
+          <h1>SIGN UP</h1>
+          <p>Explore the world from your bed with a single click</p>
+        </header>
+        <div class="inputs">
+          <div class="full-name">
+            <div class="fname-input">
+              <img src="../assets/icons/user1.svg" alt="first name icon">
+              <input type="text" placeholder="First name" required>
+            </div>
+
+            <div class="lname-input">
+              <img src="../assets/icons/user1.svg" alt="last name icon">
+              <input type="text" placeholder="Last name" required>
+            </div>
+          </div>
+
+          <div class="email-input">
+            <img src="../assets/icons/email.svg" alt="email icon">
+            <input type="email" placeholder="Enter your email" required>
+          </div>
+
+          <div class="password-input">
+            <img src="../assets/icons/password.svg" alt="password icon">
+            <input type="password" placeholder="Enter your password" required>
+            <img src="../assets/icons/eye.svg" alt="show toggle icon">
+          </div>
+
+        </div>
+        <div class="privacy-check">
+          <input type="checkbox" required>
+          <p>I accept the <a href="#">privacy policy</a></p>
+        </div>
+        <div class="cta">
+
+          <input type="submit" value="Create my account">
+          <p>Already have an account?<a href="./register.php">Log in</a></p>
+        </div>
+      </form>
+      <footer>
+        <div class="two-cols">
+          <p>© Copyright, 2024 All rights reserved to Voxworld </p>
+          <div class="right">
+            <ul>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Help</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+    
+
+  </div>
+</body>
+</html>
