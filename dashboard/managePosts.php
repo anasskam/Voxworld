f2ee3b6c443027c6043d9dd703b3b4b0db174501
anasskam <anasskam361@gmail.com>
2024-06-01 06:58:36 +0100

    added the post component structure

--- dashboard/managePosts.php
+++ dashboard/managePosts.php
@@ -22,7 +22,7 @@ $adminId = checkAdminSession();
 </head>
 <body>
 
-    <div class="dashboard-container">
+<div class="dashboard-container">
         <!-- side bar --> 
         <?php
             include "../components/sidebar.php"
@@ -34,7 +34,50 @@ $adminId = checkAdminSession();
                 include "../components/dashboard-header.php"
             ?>
 
-            <div class="content-container"></div>
+            <div class="content-container">
+                <p class="text-body1">
+                    Manage posts(10)
+                </p>
+            
+                <div class="posts-wrapper">
+                    <div class="post-wrapper">
+                        <!-- post image -->
+                        <img src="#" alt="post image">
+
+                        <div class="post-content">
+                            <p class="text-body1 text-md" value>The unseen of spending three years at Pixelgrade...</p>
+
+                            <div class="post-category-date">
+                                <span class="chip1 category">Politics</span>
+                                <span class="divider"></span>
+                                <p class="text-caption1 post-date">Feb 2, 2024 19:28 <span>(Updated: Feb 4, 2024 20:51)</span></p>
+                            </div>
+
+                            <div class="post-intractions-wrapper">
+                                <div class="post-views-wrapper">
+                                    <img src="../assets/icons/show-pass.svg" alt="views">
+                                    <span class="post-views" name="post-views">2,423 </span>views
+                                </div>
+
+                                <div class="post-likes-wrapper">
+                                    <img src="../assets/icons/like.svg" alt="likes">
+                                    <span class="post-likes" name="post-likes">215 </span>likes
+                                </div>
+
+                                <div class="post-comments-wrapper">
+                                    <img src="../assets/icons/comment.svg" alt="views">
+                                    <span class="post-comments" name="post-comments">6 </span>comments
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="post-actions">
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </main>
     </div>
 
