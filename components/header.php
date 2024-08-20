e0de069e0c68ad63add34b773ea6dfe2ddd07c91
anasskam <anasskam361@gmail.com>
2024-08-20 23:34:31 +0100

    add search component

--- components/header.php
+++ components/header.php
@@ -28,22 +28,11 @@ if (isset($_SESSION['user_id'])) {
 <header class="main-header">
   <div class="upper-header">
     <a href="./index.php"><img alt="logo" class="logo home-logo"></a>
-    <form method="POST">
-      <div class="search-bar-wrapper">
-        <div class="input-field">
-          <!-- search icon -->
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="opacity-40">
-            <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M22 22L20 20" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
 
-          <input type="text" placeholder="Search for news, articles, and more..." name="search" value="">
-
-          <button class="ghost-btn">Search</button>
-          
-        </div>
-      </div>
-    </form>
+    <!-- search bar -->
+    <?php 
+    include 'search.php'
+    ?>
 
     <?php 
       if($user_id){
